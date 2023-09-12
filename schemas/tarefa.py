1d195a4909e8a3ce27833589d91d60df2d558fba
Bruno Bastos <brunomauro@gmail.com>
2023-09-11 23:46:24 -0300

    cidade opcional

--- schemas/tarefa.py
+++ schemas/tarefa.py
@@ -12,7 +12,7 @@ class TarefaSchema(BaseModel):
     titulo: str = Field(example="Comprar pão", description="O título da tarefa")
     descricao: str = Field(example="Ir na padaria da esquina", description="A descrição da tarefa")
     data_tarefa: datetime = Field(default=datetime.now(), description="A data da tarefa", nullable=True)
-    cidade: str = Field(example="Rio de Janeiro", description="A cidade da tarefa", nullable=True)
+    cidade: Optional[str] = Field(example="Rio de Janeiro", description="A cidade da tarefa")
     status: Status = Field(example=Status.PENDENTE, description="O status da tarefa")
     prioridade: Prioridade = Field(example=Prioridade.ALTA, description="A prioridade da tarefa")
     usuario_id: Optional[int] = Field(example=1, description="O id do usuário que criou a tarefa")
